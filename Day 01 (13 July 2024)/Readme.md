ee69c8c0a81980f7d0cd0a8640a8f933b84dcc90
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-07-14 20:24:53 +0500

    Add files via upload

---
+++ Day 01 (13 July 2024)/Readme.md
@@ -0,0 +1,2 @@
+# Variables & Data Types
+
---
+++ Day 01 (13 July 2024)/script.js
@@ -0,0 +1,58 @@
+// Var declaration and print value to the console
+
+var a = 34;
+
+console.log(a);
+
+// Let declaration and print value to the console
+
+let b = "Owais Nadeem";
+
+console.log(b);
+
+// constant declaration and print value to the console
+
+const c = true;
+
+console.log(c);
+
+// Create variables of different data types (number , string , boolean , Array & Object) and print their types into the console
+
+let d = 123; // Number
+
+console.log(typeof d);
+
+let e = "Hello World!"; // String
+
+console.log(typeof e);
+
+let f = true; // Boolean
+
+console.log(typeof f);
+
+let g = [1, 2, 3, 4, 5]; // Array
+
+console.log(typeof g);
+
+let h = { name: "John Doe", age: 30 }; // Object
+
+console.log(typeof h);
+
+// Reassigning values to let
+
+let i = 56;
+console.log(i);
+
+// Reassigning values to let
+
+j = 34;
+console.log(j);
+
+// Now , reassigning the values to const and console it to see the error
+
+const k = 452;
+
+console.log(k);
+
+k = 31;
+console.log(k);
